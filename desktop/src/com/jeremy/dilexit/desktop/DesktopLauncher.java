2b234b2469b07c2301ea8521b632108d48eb7edc
Jeremy Bankes <jeremy.bankes@gmail.com>
2020-07-11 18:44:23 -0300

    Initial commit

---
+++ desktop/src/com/jeremy/dilexit/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.jeremy.dilexit.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.jeremy.dilexit.Dilexit;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Dilexit(), config);
+	}
+}
